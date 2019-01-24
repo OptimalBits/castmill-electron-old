c0f5967e7b885a8e935b0688653d6e03466f786e
Aron Kornhall <aron@kornhall.se>
2019-01-24 22:41:10 +0100

    feat: use player configurations instead of hard coded values

---
+++ src/config/castmill.ts
@@ -0,0 +1,10 @@
+import {Config} from '@/interfaces/config';
+
+const config: Config = {
+  playerUrl: 'https://player.castmill.com',
+  logUrl:  'https://player.castmill.com/log',
+  connectionCheckUrl: 'https://player.castmill.com/log',
+  updateCheckUrl: 'http://10.0.0.44:4455/updates.json', //TODO
+};
+
+export default config;
---
+++ src/config/whitelabel.ts
@@ -0,0 +1,10 @@
+import {Config} from '@/interfaces/config';
+
+const config: Config = {
+  playerUrl: 'https://player.exactbytes.com',
+  logUrl:  'https://player.exactbytes.com/log',
+  connectionCheckUrl: 'https://player.exactbytes.com/log',
+  updateCheckUrl: 'http://10.0.0.44:4455/updates.json', //TODO
+};
+
+export default config;
