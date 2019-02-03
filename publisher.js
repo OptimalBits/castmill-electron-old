2dc4ae87d8a86888cdedd580bf1bf842214d06e3
Aron Kornhall <aron@kornhall.se>
2019-02-03 23:17:11 +0100

    feat: add scripts for publishing latest build to s3

--- publisher.js
+++ publisher.js
@@ -1,10 +1,8 @@
 module.exports = {
   transport: {
-    module: 'local',
-    remoteUrl: 'https://castmill.io/updates',
-    outPath: 'dist_electron/publish',
+    module: 's3',
+    bucket: 'castmill-updates',
+    pathPrefix: 'linux/',
   },
-  // If you've already set package.json:updater.url you can skip this option:
-  updatesJsonUrl: 'https://castmill.io/updates/updates.json',
-  // updatesJsonUrl: 'http://10.0.0.44:4455/updates.json',
+  updatesJsonUrl: 'https://update.castmill.io/linux/updates.json',
 };
