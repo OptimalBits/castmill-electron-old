8079e20986022a5d43cf0a553fe093c947d28aba
Manuel Astudillo <manuel@optimalbits.com>
2019-02-03 15:23:03 +0100

    fix: add missing fields to builder options

--- vue.config.js
+++ vue.config.js
@@ -2,22 +2,31 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       builderOptions: {
-        appId: "com.castmill.player",
-        productName: "Castmill Player",
-        copyright: "(c) 2011-2018 Optimal Bits Sweden AB",
-        artifactName: "${name}_${version}_${arch}.${ext}",
+        appId: 'com.castmill.player',
+        productName: 'Castmill Player',
+        copyright: '(c) 2011-2018 Optimal Bits Sweden AB',
+        artifactName: '${name}_${version}_${arch}.${ext}',
         appImage: {
-          systemIntegration: "doNotAsk"
+          systemIntegration: 'doNotAsk',
+          category: 'AudioVideo',
         },
         // options placed here will be merged with default configuration and passed to electron-builder
         linux: {
-          category: "Video",
+          category: 'Video',
           target: [
             /*
             {
-              target: "AppImage",
-              arch: ["armv7l", "ia32", "x64"]
+              target: 'pacman',
+              arch: ['x64'],
             },
+            */
+
+            {
+              target: 'AppImage',
+              arch: ['x64'],
+              // arch: ["armv7l", "ia32","x64"]
+            },
+            /*
             {
               target: "pacman",
               arch: ["armv7l"]
@@ -26,11 +35,11 @@ module.exports = {
               target: "apk",
               arch: ["armv7l"]
             },
-            */
             {
               target: 'apk',
               arch: ['armv7l'],
             },
+            */
           ],
         },
       },
